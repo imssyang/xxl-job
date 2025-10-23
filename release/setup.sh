31700107a0e1c475edc59e927e0831bef78ec41b
imssyang <imssyang@gmail.com>
2025-10-23 16:16:35 +0800

    add xxl-job-executor

--- release/setup.sh
+++ release/setup.sh
@@ -2,8 +2,10 @@
 
 PROJ_DIR=$(dirname $(dirname $(readlink -f "$0")))
 PROJ_NAME=$(basename $PROJ_DIR)
+RELE_DIR=$PROJ_DIR/release
 SYSD_DIR=/etc/systemd/system
 SERV_ADMIN=xxl-job-admin.service
+SERV_EXECUTOR=xxl-job-executor.service
 
 debug() { echo "+ $*"; "$@"; }
 
@@ -33,12 +35,18 @@ disable_service() {
   symlink_delete $SYSD_DIR/$1
 }
 
-run_xxl_job_admin() {
-  xxl_dir=$PROJ_DIR/release
+run_admin() {
   debug java -Xms512m -Xmx1024m \
-    -DLOG_HOME=$xxl_dir/logs \
-    -Dspring.config.location=classpath:/application.properties,file:$xxl_dir/xxl-job-admin.properties \
-    -jar $xxl_dir/xxl-job-admin-3.2.1.jar
+    -DLOG_HOME=$RELE_DIR/logs \
+    -Dspring.config.location=classpath:/application.properties,file:$RELE_DIR/xxl-job-admin.properties \
+    -jar $RELE_DIR/xxl-job-admin-3.2.1.jar
+}
+
+run_executor() {
+  debug java -Xms512m -Xmx1024m \
+    -DLOG_HOME=$RELE_DIR/logs \
+    -Dspring.config.location=classpath:/application.properties,file:$RELE_DIR/xxl-job-executor.properties \
+    -jar $RELE_DIR/xxl-job-executor-3.2.1.jar
 }
 
 case "$1" in
@@ -47,13 +55,22 @@ case "$1" in
     echo "Home: $PROJ_DIR"
     ;;
   run)
-    run_xxl_job_admin $@
+    shift; name=$1; shift
+    if [[ "$name" == "admin" ]]; then
+      run_admin $@
+    elif [[ "$name" == "executor" ]]; then
+      run_executor $@
+    else
+      echo "Invalid params: $name"
+    fi
     ;;
   install)
     enable_service $SERV_ADMIN
+    enable_service $SERV_EXECUTOR
     ;;
   uninstall)
     disable_service $SERV_ADMIN
+    disable_service $SERV_EXECUTOR
     ;;
   *)
     echo "Usage: ${0##*/} {show|run|install|uninstall}"
